8a0f30b80979cf93b8d3e0c096f92f50d6df8a78
Niyati <67685812+Niyati1999@users.noreply.github.com>
2021-02-11 20:02:21 +0530

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,29 +1,23 @@
-from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import speech_recognition as sr
-
-from werkzeug.datastructures import ContentRange
-from werkzeug.utils import redirect
+from flask import Flask
+from flask import request
+from flask import render_template
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key= True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0) 
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 
-@app.route('/', methods =['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-    
-if __name__ == "__main__" :
+    if request.method == "POST":
+        f = request.files['audio_data']
+        with open('audio.wav', 'wb') as audio:
+            f.save(audio)
+        print('file uploaded successfully')
+
+        return render_template('index.html', request="POST")
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
